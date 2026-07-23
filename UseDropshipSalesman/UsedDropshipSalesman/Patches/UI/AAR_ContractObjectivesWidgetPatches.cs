3374142b389b64abe03f858c51cdcf7b2874153a
IceRaptor <gkitts@gmail.com>
2026-07-23 13:01:52 -0400

    Added drop costs and upkeep costs for dropships

---
+++ UseDropshipSalesman/UsedDropshipSalesman/Patches/UI/AAR_ContractObjectivesWidgetPatches.cs
@@ -0,0 +1,39 @@
+﻿using BattleTech.Framework;
+using BattleTech.UI;
+using GraphCoroutines;
+using System;
+
+namespace UsedDropshipSalesman.Patches.UI
+{
+    [HarmonyPatch(typeof(AAR_ContractObjectivesWidget), "FillInObjectives")]
+    [HarmonyAfter("ca.jwolf.DropCostsEnhanced", "us.frostraptor.humanresources")]
+    public static class AAR_ContractObjectivesWidget_FillInObjectives
+    {
+        public static void Postfix(AAR_ContractObjectivesWidget __instance)
+        {
+            Mod.Log.Trace?.Log("==== AAR_ContractObjectivesWidget_FillInObjectives - entered.");
+
+            var currentDropshipId = Mod.ModSaveData.CurrentDropshipId;
+            Mod.Config.Dropships.TryGetValue(currentDropshipId, out DropshipConfig config);
+            if (config == null)
+            {
+                Mod.Log.Error?.Log($"Cannot find dropship with id: {currentDropshipId} - this should not happen!");
+                return;
+            }
+
+            if (config.CustomDropship.Costs.Drop != 0)
+            {
+                string formattedCosts = string.Format("{0:n0}", config.CustomDropship.Costs.Drop);
+                Mod.Log.Info?.Log($"Dropship with id: {currentDropshipId} has drop cost of: {config.CustomDropship.Costs.Drop}, formatted as: {formattedCosts}");
+                // TODO: Localize
+                string objectiveText = "Dropship Landing Fees";
+                string missionObjectiveResultString = $"{objectiveText}: ¢{formattedCosts}";
+                string uuid = "0544e6e7-b237-4ceb-8591-1d039efa2438"; // hardcoded 
+                MissionObjectiveResult missionObjectiveResult = 
+                    new MissionObjectiveResult(missionObjectiveResultString, uuid, false, true, ObjectiveStatus.Ignored, false);
+                __instance.AddObjective(missionObjectiveResult);
+
+            }
+        }
+    }
+}
---
+++ UseDropshipSalesman/UsedDropshipSalesman/Patches/UI/SGCaptainsQuartersStatusScreenPatches.cs
@@ -0,0 +1,76 @@
+﻿using BattleTech.UI;
+using BattleTech.UI.TMProWrapper;
+using BestHTTP.SocketIO;
+using FluffyUnderware.DevTools;
+using HBS.Extensions;
+using SVGImporter;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace UsedDropshipSalesman.Patches.UI
+{
+    [HarmonyPatch(typeof(SGCaptainsQuartersStatusScreen), "RefreshData")]
+    static class SGCaptainsQuartersStatusScreen_RefreshData
+    {
+        static void Postfix(SGCaptainsQuartersStatusScreen __instance)
+        {
+            Mod.Log.Trace?.Log("==== SGCaptainsQuartersStatusScreen_RefreshData - entered.");
+
+            var currentDropshipId = Mod.ModSaveData.CurrentDropshipId;
+            Mod.Log.Info?.Log($"Current dropship is: '{currentDropshipId}', captain's quarter info.");
+            Mod.Config.Dropships.TryGetValue(currentDropshipId, out DropshipConfig config);
+            if (config == null)
+            {
+                Mod.Log.Error?.Log($"Cannot find dropship with id: {currentDropshipId} - this should not happen!");
+                return;
+            }
+
+            // Walk the SectionOneExpensesList, replacing string values
+            IEnumerator enumerator = __instance.SectionOneExpensesList.GetEnumerator();
+            try
+            {
+                while (enumerator.MoveNext())
+                {
+                    object obj = enumerator.Current;
+                    Transform transform = (Transform)obj;
+                    SGKeyValueView component = transform.gameObject.GetComponent<SGKeyValueView>();
+
+                    Mod.Log.Trace?.Log($"SGCQSS:RD - Reading key from component:{component.name}.");
+                    TextMeshProUGUI keyText = component.Key;
+                    string key = keyText.text;
+                    Mod.Log.Trace?.Log($"SGCQSS:RD - key found as: {key}");
+
+                    if (String.Equals(key, "Argo Operating Costs", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        // TODO: Localize
+                        keyText.text = $"{config.CustomDropship.Description.Name} Upkeep Costs";
+                        continue;
+                    }
+                    
+                    foreach (ShipModuleUpgrade smu in __instance.simState.shipUpgrades)
+                    {
+                        if (String.Equals(smu?.Description?.Name, key, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            // Found a ship module upgrade, prefix it
+                            // TODO: Localize
+                            keyText.text = "SHIPUPGRADE: " + key;
+                        }
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Mod.Log.Warning?.Log($"Failed to substitute key-value pairs on SGCaptainsQuartersStatusScreen: {e.Message}");
+            }
+        }
+    }
+}
